5d4e1a4c9355e464786ae66c6cad2f8b956b8d96
implaymo <136201112+implaymo@users.noreply.github.com>
2023-11-05 21:50:28 +0000

    Finished project. Automated motivational quote to email

--- Birthday Wisher (Day 32) start/main.py
+++ Birthday Wisher (Day 32) start/main.py
@@ -1,22 +1,32 @@
 import smtplib
 import datetime as dt
 import random
+from dotenv import load_dotenv
+import os
 
-my_email = "pythonuser112@gmail.com"
-password = "vuvemheklbeqqahu"
+
+def configure():
+    load_dotenv()
+
+
+configure()
+
+my_email = os.getenv("my_email")
+password = os.getenv("password")
 
 now = dt.datetime.now()
 day = now.weekday()
+
 
 with open("quotes.txt") as file:
     list_of_quotes = [line.rstrip() for line in file]
     random_quote = random.choice(list_of_quotes)
 
-if day == 1:
+if day == day:
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=my_email, password=password)
         connection.sendmail(from_addr=my_email,
-                            to_addrs="pythonuser112@gmail.com",
+                            to_addrs=my_email,
                             msg=f"Subject:Motivational quote of the day\n\n{random_quote}")
 
